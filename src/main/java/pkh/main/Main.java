e3e53d706b6afa204d42d8c88503a1b290ec2fd4
pkh <kwangho410@dreamsecurity.com>
2023-10-04 15:48:49 +0900

    [Util 추가] 1. byte util 소스 및 테스트코드 추가 (sha256 암호화 등 )

--- src/main/java/pkh/main/Main.java
+++ src/main/java/pkh/main/Main.java
@@ -1,15 +1,19 @@
 package pkh.main;
 
+import pkh.utils.ByteUtil;
 import pkh.constants.RESULT;
 
 public class Main {
     public static void main(String[] args) {
+
+        // 열거형 테스트
         System.out.println("열거형(enum) 테스트");
 
         RESULT result = null;
         result = RESULT.SUCCESS;
 
         System.out.println("열거형 타입 변수 정의 및 데이터 입력 테스트 : "+result);
+
 
     }
 }
